afca92b191e5e59807c32f0fe2101ba4731039d1
Ryan Alyn Porter <rap@endymion.com>
2020-09-01 11:18:47 -0400

    Fixed spec: The response is not cached, only the data.

--- spec/single_platform_spec.rb
+++ spec/single_platform_spec.rb
@@ -24,7 +24,7 @@ describe "Single Platform" do
     end
 
   end
-  
+
   context "caches menu data", :vcr do
 
     it 'stores the JSON data after getting it from the API' do
@@ -42,20 +42,20 @@ describe "Single Platform" do
 
     let :cached_data do
       <<-DATA
-{"code":200,"data": {"menus":[{"id":1,"menu_type":"Menu","location_id":"hakkasan-mayfair","name":"Test"}]}}
+{"menus":[{"id":1,"menu_type":"Menu","location_id":"hakkasan-mayfair","name":"Test"}]}
 DATA
     end
 
     let(:empty_response) { "{}" }
 
     it 'uses cached data instead of making an API request' do
-      
+
       expect(File).to receive(:exist?).with('cache/hakkasan-mayfair.json').and_return(true)
       expect(File).to receive(:read).with('cache/hakkasan-mayfair.json').and_return(cached_data)
       allow(File).to receive(:write).with('cache/hakkasan-mayfair.json', anything)
-      
+
       allow(HTTParty).to receive(:get).and_return(Struct.new(:code, :body).new(200, empty_response))
-      
+
       data = @single_platform.fetch_location_data_from_api(
         location_id:'hakkasan-mayfair', 'cache':true)
 
@@ -64,9 +64,9 @@ DATA
       expect(HTTParty).not_to have_received(:get)
 
     end
-    
+
   end
-  
+
   context "menu data from file system", :vcr do
 
     let :custom_data do
@@ -78,13 +78,13 @@ DATA
     let(:empty_response) { "{}" }
 
     it 'uses custom data instead of making an API request' do
-      
+
       expect(File).to receive(:exist?).with('data/hakkasan-mayfair.json').and_return(true)
       expect(File).to receive(:read).with('data/hakkasan-mayfair.json').and_return(custom_data)
       allow(File).to receive(:write).with('data/hakkasan-mayfair.json', anything)
-      
+
       allow(HTTParty).to receive(:get).and_return(Struct.new(:code, :body).new(200, empty_response))
-      
+
       data = @single_platform.fetch_location_data_from_api(
         location_id:'hakkasan-mayfair', 'data_file':'data/hakkasan-mayfair.json')
 
@@ -93,7 +93,7 @@ DATA
       expect(HTTParty).not_to have_received(:get)
 
     end
-    
+
   end
 
 end
