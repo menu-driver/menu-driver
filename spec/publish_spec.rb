d79ab98ec8b5b9c625309575560e11ea46477700
Ryan Alyn Porter <rap@endymion.com>
2020-12-01 14:53:54 -0500

    Trying to fix the CI environment.

--- spec/publish_spec.rb
+++ spec/publish_spec.rb
@@ -4,12 +4,12 @@ require_relative '../lambda'
 describe "publish" do
 
   context 'asset files', :vcr do
-    
+
     it 'uploads to S3', type: feature do
-      
+
       expect_any_instance_of(Aws::S3::Bucket).
         to receive(:object).with('hakkasan-mayfair/index.html').and_return(
-          Aws::S3::Resource.new.bucket('menu-driver-test').
+          Aws::S3::Resource.new.bucket('test.menus.hakkasangroup.com').
           object('hakkasan-mayfair/index.html'))
 
       SinglePlatform.new(
@@ -18,9 +18,9 @@ describe "publish" do
           ).publish_file(
             'hakkasan-mayfair/index.html',
             'lorem ipsum')
-      
+
     end
-    
+
   end
 
   context "main HTML menu file", :vcr do
@@ -39,22 +39,22 @@ describe "publish" do
   end
 
   context 'assets', :vcr do
-    
+
     it 'copies all asset files from the theme', type: feature do
-      
+
       single_platform = SinglePlatform.new(
             client_id: ENV['SINGLE_PLATFORM_CLIENT_ID'],
             secret:    ENV['SINGLE_PLATFORM_CLIENT_SECRET']
           )
-      
+
       object = Aws::S3::Resource.new.bucket('menu-driver-test').object('example')
       allow(single_platform).to receive(:publish_file).with(anything, anything).and_return(object)
       expect(single_platform).to receive(:publish_file).with('hakkasan-mayfair/scripts.js', anything).and_return(object)
-          
+
       single_platform.publish_menu_content(location_id:'hakkasan-mayfair')
 
     end
-    
+
   end
-  
+
 end
